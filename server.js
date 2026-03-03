e71afbc4cfd76078548596dd1522e701838c7b1c
Asir Ozturk <ahmetasiro50@gmail.com>
2026-03-03 19:38:11 +0300

    Add privacy policy html

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // ─── Routes ──────────────────────────────────────────────────────────────────
 app.use('/api/videos', videoRoutes);
